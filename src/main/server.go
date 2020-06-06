032f067b8dd254cf88a62bcb04504de433578d71
MarkoBursic <marko.bursic73@gmail.com>
2020-06-06 16:25:11 +0200

    apply master changes

--- src/main/server.go
+++ src/main/server.go
@@ -40,10 +40,12 @@ import (
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/MarkoBursic/SmartPi/src/smartpi"
-//	"github.com/prometheus/client_golang/prometheus"
-//	"github.com/prometheus/common/version"
-	// "golang.org/x/net/context"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
 )
+
 
 type JSONMessage struct {
 	Code    int    `json:"code"`
@@ -67,6 +69,15 @@ var etagHeaders = []string{
 	"If-Range",
 	"If-Unmodified-Since",
 }
+
+var responseCount = promauto.NewCounterVec(
+	prometheus.CounterOpts{
+		Namespace: "smartpi",
+		Name:      "responses_total",
+		Help:      "Total HTTP requests processed by the server, excluding scrapes.",
+	},
+	[]string{"code", "method"},
+)
 
 func NoCache(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
@@ -132,7 +143,7 @@ func BasicAuth(realm string, handler http.HandlerFunc, c *smartpi.Config, u *sma
 }
 
 func init() {
-	//prometheus.MustRegister(version.NewCollector("smartpi"))
+	prometheus.MustRegister(version.NewCollector("smartpi"))
 }
 
 var appVersion = "No Version Provided"
@@ -186,7 +197,7 @@ func main() {
 	// r.HandleFunc("/api/config/network/wifi/active/{name}", BasicAuth("Please enter your username and password for this site", smartpi.DeactivateWifi, config, user, "smartpiadmin")).Methods("DELETE")
 	// r.HandleFunc("/api/config/network/wifi/security/change/key", BasicAuth("Please enter your username and password for this site", smartpi.ChangeWifiKey, config, user, "smartpiadmin")).Methods("POST")
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(config.DocRoot)))
-	//http.Handle("/metrics", prometheus.Handler())
-	//http.Handle("/", prometheus.InstrumentHandler("smartpi", r))
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/", promhttp.InstrumentHandlerCounter(responseCount, r))
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(config.WebserverPort), nil))
 }
